f839f739598a1d1d0be20d1347db9cda90915ccd
plang007 <plang760@hotmail.com>
2019-07-12 23:51:27 -0700

    define the struct for post and location

--- service/main.go
+++ service/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
+	User     string   `json:"user"`
+	Message  string   `json:"message"`
+	Location Location `json:"location"`
+}
+
 func main() {
 	fmt.Println("Hello, world")
 }
